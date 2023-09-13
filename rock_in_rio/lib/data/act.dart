8d54294dd3c3500abcc1b3c3a7a2b32df0be1f9a
hermeshcg <hermescaretta@gmail.com>
2023-09-12 21:29:44 -0300

    refactor

---
+++ rock_in_rio/lib/data/act.dart
@@ -0,0 +1,23 @@
+class Act {
+  final String name;
+  final int day;
+  final List<String> tags;
+
+  // Construtor da classe
+  const Act(this.name, this.day, this.tags);
+}
+
+const actsList = [
+  Act("Iron Maiden", 2, ["Espetaculo", "Fas", "NovoAlbum"]),
+  Act("Alok", 3, ["Influente", "Top", "Show"]),
+  Act("Justin Bieber", 4, ["TopCharts", "Hits", "PríncipeDoPOP"]),
+  Act("Guns N’ Roses", 8, ["Sucesso", "Espetáculo", "Fas"]),
+  Act("Capital Inicial", 9, ["2019", "Novo Álbum", "Fas"]),
+  Act("Green Day", 9, ["Sucesso", "Reconhecimento", "Show"]),
+  Act("Cold Play", 10, ["NovoAlbum", "Sucesso", "2011"]),
+  Act("Ivete Sangalo", 10, ["Unica", "Carreiras", "Fas"]),
+  Act("Racionais", 3, ["Hits", "Prêmios", "Respeito"]),
+  Act("Gloria Groove", 8, ["Streams", "Representatividade", "Sucesso"]),
+  Act("Avril Lavigne", 9, ["Estreia", "Sucesso", "Lançamento"]),
+  Act("Ludmilla", 10, ["Representativade", "Sucesso", "Parcerias"]),
+];
